8a558bebe0c9b90c162325d81de1a10685f91b72
Sravani <sravanijanak@gmail.com>
2026-03-17 12:47:53 +0530

    db path changes

--- backend/app/database/db_setup.py
+++ backend/app/database/db_setup.py
@@ -1,17 +1,13 @@
+
+
 import pandas as pd
 import sqlite3
 import os
 
-# Get project root
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# Go to project root
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../"))
 
-# Go up to root folder
-ROOT_DIR = os.path.abspath(os.path.join(BASE_DIR, "../../../"))
-
-# Correct data path
 DATA_PATH = os.path.join(ROOT_DIR, "data", "nykaa_marketing.csv")
-
-# DB path
 DB_PATH = os.path.join(ROOT_DIR, "marketing.db")
 
 
@@ -39,7 +35,33 @@ columns = [
     "Date"
 ]
 
+# def load_csv_to_db():
+
+#     df = pd.read_csv(
+#         DATA_PATH,
+#         skiprows=2,
+#         header=None,
+#         names=columns,
+#         encoding="latin1"
+#     )
+
+#     conn = sqlite3.connect(DB_PATH)
+
+#     df.to_sql(
+#         "campaigns",
+#         conn,
+#         if_exists="replace",
+#         index=False
+#     )
+
+#     conn.close()
+
+#     print("Database created successfully!")
 def load_csv_to_db():
+
+    print("CSV PATH:", DATA_PATH)
+    print("CSV EXISTS:", os.path.exists(DATA_PATH))
+    print("DB PATH:", DB_PATH)
 
     df = pd.read_csv(
         DATA_PATH,
@@ -64,3 +86,5 @@ def load_csv_to_db():
 
 if __name__ == "__main__":
     load_csv_to_db()
+
+
